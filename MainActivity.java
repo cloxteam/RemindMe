7531235a3239a0ee798ed55fb2cdb45f409e1885
Artem Merezhko <cloxteam@gmail.com>
2016-09-15 20:12:11 +0300

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package com.qubico.remindme;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by artya on 15.09.16.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
